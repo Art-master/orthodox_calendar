2c310b57b612b96859e074bca73eb8eb7861946d
Art-master <ggn.master@gmail.com>
2022-09-28 22:00:56 +0400

    (compose migration) modify JustifiedText for compose

--- app/src/main/java/com/artmaster/android/orthodoxcalendar/ui/review/components/HolidayPage.kt
+++ app/src/main/java/com/artmaster/android/orthodoxcalendar/ui/review/components/HolidayPage.kt
@@ -1,10 +1,7 @@
 package com.artmaster.android.orthodoxcalendar.ui.review.components
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
@@ -13,22 +10,22 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.res.ResourcesCompat
 import com.artmaster.android.orthodoxcalendar.R
 import com.artmaster.android.orthodoxcalendar.domain.Holiday
 import com.artmaster.android.orthodoxcalendar.ui.CalendarViewModel
@@ -42,7 +39,7 @@ import com.artmaster.android.orthodoxcalendar.ui.tile_calendar.components.Orname
 fun HolidayPagePreview() {
     val holiday = Holiday(
         title = "Самый большой праздник",
-        description = "Очень длинное описание для праздника"
+        description = "Очень длинное описание для праздника. Очень длинное описание для праздника. Очень длинное описание для праздника.Очень длинное описание для праздника. Очень длинное описание для праздника. Очень длинное описание для праздника. Очень длинное описание для праздника"
     )
 
     val data = remember { mutableStateOf<Holiday?>(holiday) }
@@ -122,37 +119,41 @@ fun HolidayPageTitle(holiday: Holiday, headerHeight: Dp = 80.dp) {
 @Composable
 fun HolidayDescription(holiday: Holiday) {
 
-    val description = if (holiday.description.isEmpty()) {
-        buildAnnotatedString { append(stringResource(id = R.string.no_description)) }
-    } else buildAnnotatedString {
-        HeadSymbol(
-            builder = this,
-            text = holiday.description.first()
-        )
-        append(holiday.description.substring(1))
-    }
+    val justifiedTextViewCompose = remember { JustifiedTextViewCompose() }
 
-    Text(
-        modifier = Modifier.padding(start = 5.dp, top = 10.dp, end = 5.dp),
-        color = DefaultTextColor,
-        text = description,
-        fontSize = 20.sp,
-        fontFamily = FontFamily(Font(R.font.cyrillic_old)),
-        textAlign = TextAlign.Justify
-    )
+    Box(modifier = Modifier.padding(top = 15.dp, start = 5.dp, end = 5.dp)) {
+        HeadSymbol(text = holiday.description.first().toString())
+
+        val context = LocalContext.current
+
+        Canvas(modifier = Modifier.fillMaxWidth(), onDraw = {
+
+            drawContext.canvas.nativeCanvas.apply {
+                justifiedTextViewCompose.apply {
+                    setText(holiday.description.substring(startIndex = 1))
+                    setCanvasSize(size)
+
+                    val typeface = ResourcesCompat.getFont(context, R.font.cyrillic_old)
+                    setTypeface(typeface!!)
+                    setTextColor(DefaultTextColor)
+                    setRawTextSize(20.dp.roundToPx())
+                    setLeadingMargin(3, 6)
+                }
+                justifiedTextViewCompose.onDraw(this)
+
+            }
+        })
+    }
 }
 
 @Composable
-fun HeadSymbol(builder: AnnotatedString.Builder, text: Char) {
-    builder.apply {
-        withStyle(
-            style = SpanStyle(
-                color = HeadSymbolTextColor,
-                fontFamily = FontFamily(Font(R.font.bukvica)),
-                fontSize = 40.sp
-            )
-        ) {
-            append(text)
-        }
-    }
+fun HeadSymbol(text: String) {
+    Text(
+        modifier = Modifier.size(60.dp),
+        text = text,
+        color = HeadSymbolTextColor,
+        fontFamily = FontFamily(Font(R.font.bukvica)),
+        fontSize = 60.sp,
+        textAlign = TextAlign.Center
+    )
 }
